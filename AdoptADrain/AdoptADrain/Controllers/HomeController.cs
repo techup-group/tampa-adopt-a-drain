0f104dcbba8cbf2ab74b64a430b04f273fcfbd71
fplaras <1815587@fplaras@users.noreply.github.com>
2020-02-24 20:23:00 -0500

    added some images and credit page and links for later use

--- AdoptADrain/AdoptADrain/Controllers/HomeController.cs
+++ AdoptADrain/AdoptADrain/Controllers/HomeController.cs
@@ -28,10 +28,17 @@ namespace AdoptADrain.Controllers
             return View();
         }
 
+        public IActionResult CreditTo()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        
     }
 }
