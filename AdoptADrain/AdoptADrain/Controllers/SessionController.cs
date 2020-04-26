78754355cc534def1d18303eb9eedeb469d15446
fplaras <1815587@fplaras@users.noreply.github.com>
2020-04-26 14:57:04 -0400

    fixed maps loading from home page, removed sign out page

--- AdoptADrain/AdoptADrain/Controllers/SessionController.cs
+++ AdoptADrain/AdoptADrain/Controllers/SessionController.cs
@@ -51,21 +51,10 @@ namespace AdoptADrain.Controllers
         [HttpGet]
         public IActionResult SignOut()
         {
-            var callbackUrl = Url.Action(nameof(SignedOut), "Session", values: null, protocol: Request.Scheme);
+            var callbackUrl = Url.Action(nameof(HomeController.Index), "Home", values: null, protocol: Request.Scheme);
             return SignOut(new AuthenticationProperties { RedirectUri = callbackUrl },
                 CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme);
         }
 
-        [HttpGet]
-        public IActionResult SignedOut()
-        {
-            if (User.Identity.IsAuthenticated)
-            {
-                // Redirect to home page if the user is authenticated.
-                return RedirectToAction(nameof(HomeController.Index), "Home");
-            }
-
-            return View();
-        }
     }
 }
